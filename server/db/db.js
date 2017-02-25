f733fb0b44cf4479450bea26277014efa176d2d9
seanng <shonum@gmail.com>
2017-02-25 20:39:39 +0800

    Update DB schema

--- server/db/db.js
+++ server/db/db.js
@@ -2,52 +2,75 @@ const Sequelize = require('sequelize');
 
 // placeholder username and password
 // TODO: move pw and username to a separate settings file
-const sequelize = new Sequelize('haven', 'root', 'Ca$hmere1');
+const sequelize = new Sequelize('haven', 'root', 'Ca$hmere1', {
+  dialect: 'postgres',
+});
 
 const Customer = sequelize.define('Customer', {
-	username: Sequelize.STRING,
-	password: Sequelize.STRING,
-	firstName: Sequelize.STRING,
-	lastName: Sequelize.STRING,
-	email: Sequelize.STRING,
-	phoneNo: Sequelize.STRING,
-	accountStatus: Sequelize.INTEGER,
-	paymentAuthStatus: Sequelize.INTEGER,
-	stripeKey: Sequelize.STRING,
-	regDate: Sequelize.DATE,
-	rating: Sequelize.DECIMAL
+  regDate: Sequelize.DATE,
+  username: Sequelize.STRING,
+  password: Sequelize.STRING,
+  firstName: Sequelize.STRING,
+  lastName: Sequelize.STRING,
+  email: Sequelize.STRING,
+  phoneNo: Sequelize.STRING,
+  accountStatus: Sequelize.INTEGER,
+  paymentAuthStatus: Sequelize.INTEGER,
+  stripeKey: Sequelize.STRING, // <-- this needs to be looked into further
+  rating: Sequelize.DECIMAL,
 });
 
 const Hotel = sequelize.define('Hotel', {
-	name: Sequelize.STRING,
-	description: Sequelize.STRING,
-	lat: Sequelize.DECIMAL,
-	long: Sequelize.DECIMAL,
-	rating: Sequelize.DECIMAL,
-	availableRooms: Sequelize.INTEGER,
-	rate: Sequelize.DECIMAL
+  regDate: Sequelize.DATE,
+  name: Sequelize.STRING,
+  description: Sequelize.TEXT,
+  photos: Sequelize.ARRAY(Sequelize.STRING), // array[0] would be the primary photoURL
+  lat: Sequelize.DECIMAL,
+  long: Sequelize.DECIMAL,
+  address: Sequelize.STRING,
+  paymentInfo: Sequelize.JSON, // <-- this needs to be looked into further
+  rate: Sequelize.DECIMAL, // <-- hourly? or per minute?
+  rateCurrency: Sequelize.STRING,
+  rating: Sequelize.DECIMAL,
 });
 
 const Stay = sequelize.define('Stay', {
-	status: Sequelize.INTEGER,
-	date: Sequelize.DATE,
-	duration: Sequelize.INTEGER, //in minutes?
-	totalAmount: Sequelize.DECIMAL,
-	transactionStatus: Sequelize.INTEGER
+  status: Sequelize.STRING,
+  bookingTime: Sequelize.DATE,
+  checkInTime: Sequelize.DATE, // Update on check in
+  checkOutTime: Sequelize.DATE, // Update on check out
+  roomNumber: Sequelize.STRING, // Update on create Room
+  roomType: Sequelize.STRING,
+  surcharges: Sequelize.ARRAY(Sequelize.JSON),
+  /* example: [{
+      description: string
+      status: string
+      amount: decimal
+      dateOfCharge: date
+      currency: string
+    }] // Update on adding surcharge */
+  roomCharge: Sequelize.JSON,
+  /* example: {
+    status: string
+    amount: decimal
+    currency: string
+  } // Update on check out */
+  totalCharge: Sequelize.DECIMAL, // Update on any charge change
 });
 
 // many-to-many relationship between customers and hotels
-Customer.belongsToMany(Hotel, {through: Stay});
-Hotel.belongsToMany(Customer, {through: Stay});
+Customer.belongsToMany(Hotel, { through: Stay });
+Hotel.belongsToMany(Customer, { through: Stay });
 
 const Employee = sequelize.define('Employee', {
-	username: Sequelize.STRING,
-	password: Sequelize.STRING,
-	firstName: Sequelize.STRING,
-	lastName: Sequelize.STRING,
-	email: Sequelize.STRING,
-	phoneNo: Sequelize.STRING,
-	admin: Sequelize.INTEGER
+  regDate: Sequelize.DATE,
+  username: Sequelize.STRING,
+  password: Sequelize.STRING,
+  firstName: Sequelize.STRING,
+  lastName: Sequelize.STRING,
+  email: Sequelize.STRING,
+  phoneNo: Sequelize.STRING,
+  admin: Sequelize.INTEGER, // level 1: staff, level 2: admin, level 3: super-user? (i.e. customer service)
 });
 
 // one-to-many relationship
@@ -59,9 +82,9 @@ Stay.sync();
 Employee.sync();
 
 module.exports = {
-	sequelize,
-	Customer,
-	Hotel,
-	Stay,
-	Employee
+  sequelize,
+  Customer,
+  Hotel,
+  Stay,
+  Employee,
 };
