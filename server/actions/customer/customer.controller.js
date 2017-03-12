c82856ebb64610ecb01988f2bd0ee8a32dd1c880
Ching <chingyang91@gmail.com>
2017-03-12 21:01:11 +0800

    Setup user sign up

---
+++ server/actions/customer/customer.controller.js
@@ -0,0 +1,27 @@
+const Customer = require('./customer.model');
+const signToken = require('../auth/auth.service').signToken;
+const controller = {};
+
+controller.createNewCustomer = (res, rej, req, params) => {
+  const {
+    username,
+    password,
+    firstName,
+    lastName,
+    email,
+    phoneNo,
+  } = req.body;
+  return Customer.create({ username, password, firstName, lastName, email, phoneNo })
+    .then((user) => ({ token: signToken(user.id), user }))
+    .then((data) => res({ data }));
+};
+
+controller.getCustomer = (res, rej, req, params) => {
+
+};
+
+controller.putCustomer = (res, rej, req, params) => {
+  
+};
+
+module.exports = controller;
