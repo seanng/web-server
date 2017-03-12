23c4a357b245189ee7287c320f625957314f1f67
Ching <chingyang91@gmail.com>
2017-03-12 21:01:11 +0800

    Setup user sign up

---
+++ server/actions/auth/index.js
@@ -0,0 +1,8 @@
+const postAuth = (res, rej, req, params) => {
+  console.log(res, rej, req, params);
+  return res('post stay');
+}
+
+module.exports = (req, params) => new Promise((res, rej) => {
+  if (req.method === 'POST') return postAuth(res, rej, req, params);
+})
---
+++ server/actions/customer/customer.model.js
@@ -0,0 +1,12 @@
+const Customer = require('../../db/config').Customer;
+const bcrypt = require('bcrypt-nodejs');
+const Promise = require('bluebird');
+
+const cipher = Promise.promisify(bcrypt.hash);
+const compare = Promise.promisify(bcrypt.compare);
+
+Customer.beforeCreate(function(user, options) {
+  return cipher(user.password, null, null).then(function (hashedPw) {
+    user.password = hashedPw;
+  });
+})
