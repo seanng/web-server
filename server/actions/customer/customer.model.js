7fc4daf61c09c83ec1c7d6f85b8d8c7ef8859c0c
Ching <chingyang91@gmail.com>
2017-04-29 16:42:39 +0800

    Hashing user password before create along with instance methods of comparing hashed password

--- server/actions/customer/customer.model.js
+++ server/actions/customer/customer.model.js
@@ -3,10 +3,12 @@ const bcrypt = require('bcrypt-nodejs');
 const Promise = require('bluebird');
 
 const cipher = Promise.promisify(bcrypt.hash);
-// const compare = Promise.promisify(bcrypt.compare);
 
 Customer.beforeCreate((user, options) => {
+  console.log('user', user);
   return cipher(user.password, null, null).then((hashedPw) => {
     user.password = hashedPw;
   });
 });
+
+module.exports = Customer;
