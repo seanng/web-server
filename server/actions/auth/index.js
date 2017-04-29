48f51d56787591bbe5e8eb8fbaeb1d2166c19f62
Ching <chingyang91@gmail.com>
2017-04-29 14:28:48 +0800

    auth

--- server/actions/auth/index.js
+++ server/actions/auth/index.js
@@ -1,6 +1,6 @@
 const postAuth = (res, rej, req, params) => {
   console.log(res, rej, req, params);
-  return res('post stay');
+  return res('post Auth');
 }
 
 module.exports = (req, params) => new Promise((res, rej) => {
