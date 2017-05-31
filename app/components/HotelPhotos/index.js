479bef8ea6a008a1f0d42abada22a0da9dc0af52
seanng <shonum@gmail.com>
2017-05-31 21:35:49 +0800

    Preview photo on thumbnail click

--- app/components/HotelPhotos/index.js
+++ app/components/HotelPhotos/index.js
@@ -21,8 +21,9 @@ const Primary = styled.div`
 const Secondary = styled.div`
   height: 90px;
   width: 100%;
-  border: 1px dotted grey;
+  border: ${props => props.active === props.index ? '1px solid red' : '1px dotted grey'};
   margin-bottom: 15px;
+  cursor: pointer;
 `;
 
 const Img = styled.img`
@@ -48,7 +49,11 @@ const fileLabelStyle = {
 class HotelPhotos extends React.Component {
 
   state = {
-    previewPhoto: this.props.photos[0]
+    activeIndex: 0
+  }
+
+  setPhotoAsActive = (activeIndex) => {
+    this.props.photos.length > activeIndex && this.setState({ activeIndex })
   }
 
   render () {
@@ -57,7 +62,7 @@ class HotelPhotos extends React.Component {
         <div className="row">
           <div className="col-sm-9">
             <Primary>
-              <Img src={this.state.previewPhoto} />
+              <Img src={this.props.photos[this.state.activeIndex]} />
             </Primary>
           </div>
           <div className="col-sm-3" style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
@@ -67,32 +72,32 @@ class HotelPhotos extends React.Component {
                 <Glyphicon glyph='glyphicon glyphicon-plus' style={{ fontSize: 42 }} />
               </GlyphWrapper>
             </label>
-            <Secondary>
+            <Secondary index={5} active={this.state.activeIndex} onClick={this.setPhotoAsActive.bind(null, 5)}>
               { this.props.photos[5] && <Img src={this.props.photos[5]} /> }
             </Secondary>
-            <Secondary>
+            <Secondary index={4} active={this.state.activeIndex} onClick={this.setPhotoAsActive.bind(null, 4)}>
               { this.props.photos[4] && <Img src={this.props.photos[4]} /> }
             </Secondary>
           </div>
         </div>
         <div className="row">
           <div className="col-sm-3">
-            <Secondary>
+            <Secondary index={0} active={this.state.activeIndex} onClick={this.setPhotoAsActive.bind(null, 0)} >
               { this.props.photos[0] && <Img src={this.props.photos[0]} /> }
             </Secondary>
           </div>
           <div className="col-sm-3">
-            <Secondary>
+            <Secondary index={1} active={this.state.activeIndex} onClick={this.setPhotoAsActive.bind(null, 1)}>
               { this.props.photos[1] && <Img src={this.props.photos[1]} /> }
             </Secondary>
           </div>
           <div className="col-sm-3">
-            <Secondary>
+            <Secondary index={2} active={this.state.activeIndex} onClick={this.setPhotoAsActive.bind(null, 2)}>
               { this.props.photos[2] && <Img src={this.props.photos[2]} /> }
             </Secondary>
           </div>
           <div className="col-sm-3">
-            <Secondary>
+            <Secondary index={3} active={this.state.activeIndex} onClick={this.setPhotoAsActive.bind(null, 3)}>
               { this.props.photos[3] && <Img src={this.props.photos[3]} /> }
             </Secondary>
           </div>
