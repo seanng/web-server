45d585f5190d419815f07628bfeade19ac755cd3
seanng <shonum@gmail.com>
2017-06-13 17:19:15 +0800

    Add Amenity Icons

--- app/components/HotelAmenities/index.js
+++ app/components/HotelAmenities/index.js
@@ -5,25 +5,55 @@
 */
 
 import React from 'react';
-// import styled from 'styled-components';
+import styled from 'styled-components';
 
 import { FormattedMessage } from 'react-intl';
 import RightContainer from 'components/RightContainer'
 import services from './services.json';
 import messages from './messages';
 
+const Img = styled.img`
+  height: 100%;
+  width: 50%;
+`
+
+const ImgWrapper = styled.div`
+  display: block;
+  text-align: center;
+  height: 40px;
+  width: 80px;
+  padding-bottom: 10px;
+`
+
+const AmenityIconWrapper = styled.div`
+  display: inline-block;
+  text-align: center;
+`
+
+const AmenitiesWrapper = styled.div`
+  overflow: scroll;
+  padding-top: 20px;
+`
+
 const AmenityIcon = ({ amenity }) => {
-  const iconUrl = services[amenity]
+  const iconName = services[amenity]
   return (
-    <span>{ amenity }</span>
+    <AmenityIconWrapper>
+      <ImgWrapper>
+        <Img src={iconName} />
+      </ImgWrapper>
+      { amenity }
+    </AmenityIconWrapper>
   )
 }
 
 const HotelAmenities = ({ displayAmenitiesModal, amenities }) => {
   const onClick = () => displayAmenitiesModal(true)
   return (
-    <RightContainer title='amenities' topRightIcon='glyphicon glyphicon-plus-sign' onClick={onClick}>
-      { amenities.map((amenity, i) => <AmenityIcon amenity={amenity} key={i} /> )}
+    <RightContainer title='amenities' topRightIcon='glyphicon glyphicon-pencil' onClick={onClick}>
+      <AmenitiesWrapper>
+        { amenities.map((amenity, i) => <AmenityIcon amenity={amenity} key={i} /> )}
+      </AmenitiesWrapper>
     </RightContainer>
   )
 }
